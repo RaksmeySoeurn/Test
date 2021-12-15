0d8b61c56ac301b295391ad85c198cf72a24191c
Raksmey Soeurn <soeurn.raksmey14@gmail.com>
2021-12-15 22:01:59 +0700

    Add project files.

---
+++ spi_fontawesome/frm_dashboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spi_fontawesome
+{
+    public partial class frm_dashboard : Form
+    {
+        public frm_dashboard()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
